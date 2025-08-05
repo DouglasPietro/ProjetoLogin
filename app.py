6b50dc10776f631944d8824133944bd33b4ee840
DouglasPietro <101369903+DouglasPietro@users.noreply.github.com>
2025-08-05 19:13:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def sucesso():
     usuario = session.get('usuario')
     return render_template('sucesso.html', usuario=usuario)
 
-
 if __name__ == '__main__':
-    init_db()
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
